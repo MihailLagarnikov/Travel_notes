7c9786140eb99e74411a848ca0dab835b9c7b275
lagarnikov <m.lagarnikov@cft.ru>
2020-10-31 12:07:01 +0700

    переверстал регистрацию по емаилу сделал логическое разделение фрагмента на регистрацию и вход

--- app/src/main/java/com/twosmalpixels/travel_notes/ui/auth/EmailAuthFragment.kt
+++ app/src/main/java/com/twosmalpixels/travel_notes/ui/auth/EmailAuthFragment.kt
@@ -4,11 +4,16 @@ import BaseFragment
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.view.MotionEvent
 import android.view.View
+import android.view.View.OnTouchListener
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.twosmalpixels.travel_notes.R
+import com.twosmalpixels.travel_notes.core.extension.setVisibility
 import com.twosmalpixels.travel_notes.pojo.ToolbarParam
 import com.twosmalpixels.travel_notes.ui.MainActivity
 import kotlinx.android.synthetic.main.email_auth_fragment.*
@@ -16,14 +21,15 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
-class EmailAuthFragment : BaseFragment(), TextWatcher {
+class EmailAuthFragment : BaseFragment(), TextWatcher, OnTouchListener {
 
     val MIN_LENGHT_EMAIL = 6
     val MIN_LENGHT_PASSWORD = 6
 
     private lateinit var authViewModel: AuthViewModel
+    private var isRegistration = true
 
-    override fun getToolbarParam() = ToolbarParam()
+    override fun getToolbarParam() = ToolbarParam(getString(R.string.registration))
 
     override fun getLayout() = R.layout.email_auth_fragment
 
@@ -48,12 +54,39 @@ class EmailAuthFragment : BaseFragment(), TextWatcher {
         }
         button_email_auth_save.setOnClickListener {
             GlobalScope.launch {
-                createUserWithEmailAndPassword(
-                    enter_email_edit_text.text.toString(),
-                    double_password_edit_text.text.toString()
-                )
+                if (isRegistration) {
+                    createUserWithEmailAndPassword(
+                        enter_email_edit_text.text.toString().trim(),
+                        double_password_edit_text.text.toString().trim()
+                    )
+                } else {
+                    signInWithEmailAndPassword(
+                        enter_email_edit_text.text.toString().trim(),
+                        enter_password_edit_text.text.toString().trim()
+                    )
+                }
             }
         }
+        show_top_password.setOnTouchListener(this)
+        show_bottom_password.setOnTouchListener(this)
+
+        enter_bottom.setOnClickListener {
+            isRegistration = !isRegistration
+            setMode(isRegistration)
+        }
+        enter_top.setOnClickListener {
+            isRegistration = !isRegistration
+            setMode(isRegistration)
+        }
+        setMode(isRegistration)
+    }
+
+    private fun setMode(isRegistration: Boolean) {
+        double_password_card.setVisibility(isRegistration)
+        enter_top.setText(if (isRegistration) R.string.has_account else R.string.need_registration)
+        enter_bottom.setText(if (isRegistration) R.string.has_account_entry else R.string.registration_now)
+        text_fogot_password.setVisibility(!isRegistration)
+        button_email_auth_save.setText(if (isRegistration) R.string.registration_now else R.string.has_account_entry)
     }
 
     override fun afterTextChanged(p0: Editable?) {
@@ -62,8 +95,10 @@ class EmailAuthFragment : BaseFragment(), TextWatcher {
         val validBtn = (emailLenght > MIN_LENGHT_EMAIL)
                 && (enter_password_edit_text.text != null
                 && passwordLenght > MIN_LENGHT_PASSWORD
-                && double_password_edit_text.text != null
-                && enter_password_edit_text.text.toString().equals(double_password_edit_text.text.toString()))
+                && (!isRegistration || double_password_edit_text.text != null)
+                && (!isRegistration || enter_password_edit_text.text.toString().equals(
+            double_password_edit_text.text.toString()
+        )))
         button_email_auth_save.isEnabled = validBtn
     }
 
@@ -88,5 +123,59 @@ class EmailAuthFragment : BaseFragment(), TextWatcher {
         } else {
             Snackbar.make(requireView(), "Authentication Failed.", Snackbar.LENGTH_LONG).show()
         }
+    }
+
+    private suspend fun signInWithEmailAndPassword(email: String, password: String) {
+        val job = GlobalScope.async {
+            authViewModel.signInWithEmailAndPassword(
+                (activity!! as MainActivity).auth,
+                email,
+                password
+            )
+        }
+        if (job.await()) {
+            findNavController().navigate(R.id.action_emailAuthFragment_to_youTravelsFragment)
+        } else {
+            Snackbar.make(requireView(), "Authentication Failed.", Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onTouch(view: View, motionEvent: MotionEvent): Boolean {
+        when (view) {
+            show_top_password -> {
+                when (motionEvent.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        enter_password_edit_text.setTransformationMethod(
+                            HideReturnsTransformationMethod()
+                        )
+                        show_top_password.setImageResource(R.drawable.ic_eye_open)
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        enter_password_edit_text.setTransformationMethod(
+                            PasswordTransformationMethod()
+                        )
+                        show_top_password.setImageResource(R.drawable.ic_eye_close)
+                    }
+                }
+            }
+            show_bottom_password -> {
+                when (motionEvent.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        double_password_edit_text.setTransformationMethod(
+                            HideReturnsTransformationMethod()
+                        )
+                        show_bottom_password.setImageResource(R.drawable.ic_eye_open)
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        double_password_edit_text.setTransformationMethod(
+                            PasswordTransformationMethod()
+                        )
+                        show_bottom_password.setImageResource(R.drawable.ic_eye_close)
+                    }
+                }
+            }
+
+        }
+        return true
     }
 }
